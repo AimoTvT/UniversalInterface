d6809e6b1898adc5fe5a771ef002457dc76c48ae
Aimo <3232199020@qq.com>
2023-08-08 22:13:07 +0800

    取消开源描述

--- Source/UniversalInterface/Public/UniversalInterface.h
+++ Source/UniversalInterface/Public/UniversalInterface.h
@@ -1,13 +1,11 @@
 /**
  * Copyright: Aimo_皑墨
- * Open source protocol: MIT License
  * Open Source: June 29, 2023
  * making address: https://github.com/AimoTvT/UniversalInterface
  * We welcome the contributions of powerful movers and movers to join this plugin
  * Build powerful plugins together!!
  *
  * 版权所有权: Aimo_皑墨
- * 开源协议: MIT License
  * 开源时间: 2023年6月29号
  * GitHub地址: https://github.com/AimoTvT/UniversalInterface
  * 欢迎有实力的大佬/萌新加入本插件的贡献
