18098e2b43214e8acc36bf23fa9430db6af3edd3
Aimo <3232199020@qq.com>
2023-09-20 22:22:46 +0800

    更新

--- Source/UniversalInterface/Private/UniversalInterfaces.cpp
+++ Source/UniversalInterface/Private/UniversalInterfaces.cpp
@@ -1,4 +1,16 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+/**
+ * Copyright: Aimo_皑墨
+ * Open Source: June 29, 2023
+ * making address: https://github.com/AimoTvT/UniversalInterface
+ * We welcome the contributions of powerful movers and movers to join this plugin
+ * Build powerful plugins together!!
+ *
+ * 版权所有权: Aimo_皑墨
+ * 开源时间: 2023年6月29号
+ * GitHub地址: https://github.com/AimoTvT/UniversalInterface
+ * 欢迎有实力的大佬/萌新加入本插件的贡献
+ * 一起打造强大的插件!!!
+ */
 
 
 #include "UniversalInterfaces.h"
